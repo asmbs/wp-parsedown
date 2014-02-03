25c98094410a10ee19ca0e8b5dc36418f38a9ba5
Kyle Tucker <kyleatucker@gmail.com>
2014-02-03 11:21:53 -0500

    Base authoring

---
+++ wp-parsedown.php
@@ -0,0 +1,60 @@
+<?php
+// -- wp-parsedown | v0.0.1 | MIT License | @friartuck6000
+// ---------------------------------------------------------------------
+
+// Plugin Name:  WP Parsedown
+// Plugin URI:   https://github.com/friartuck6000/wp-parsedown
+// Description:  A wrapper for Parsedown that lets you use Markdown in WordPress.
+// Version:      0.0.1
+// Author:       Kyle Tucker
+// Author URI:   https://github.com/friartuck6000
+
+class WP_Parsedown
+{
+  // Parsedown instance
+  public $parser;
+
+  // Private members
+  private $path;
+  private $uri;
+
+  // Follows singleton pattern
+  private static $instance = NULL;
+
+  private function __construct()
+  {
+    // Set path and URI
+    $this->path = plugin_dir_path( __FILE__ );
+    $this->uri  = plugin_dir_url(  __FILE__ );
+
+    // Include Parsedown and get an instance of it.
+    require_once $this->path .'parsedown/Parsedown.php';
+    $this->parser = Parsedown::instance();
+
+    add_action( 'init', [ &$this, 'init' ] );
+  }
+
+  // Runs on init; adds our content filter.
+  public function init()
+  {
+    add_filter( 'the_content', [ &$this, 'parse' ], 1 );
+  }
+
+  // The actual parser; runs on the_content filter, but can be run
+  // manually too.
+  public function parse( $content )
+  {
+    return $this->parser->parse( $content );
+  }
+
+  // Get single object instance.
+  public static function instance()
+  {
+    if ( self::$instance === NULL )
+      self::$instance = new self;
+    return self::$instance;
+  }
+
+};
+
+WP_Parsedown::instance();
