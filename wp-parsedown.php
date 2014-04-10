5fd168f041e98e4e2079c83550fddcd05df191da
Kyle Tucker <kyleatucker@gmail.com>
2014-04-10 13:38:20 -0400

    Bump version for feature release

--- wp-parsedown.php
+++ wp-parsedown.php
@@ -1,11 +1,11 @@
 <?php
-// -- wp-parsedown | v0.4.0 | MIT License | @friartuck6000
+// -- wp-parsedown | v0.5.0 | MIT License | @friartuck6000
 // ---------------------------------------------------------------------
 
 // Plugin Name:  WP Parsedown
 // Plugin URI:   https://github.com/friartuck6000/wp-parsedown
 // Description:  A wrapper for Parsedown that lets you use Markdown in WordPress.
-// Version:      0.4.0
+// Version:      0.5.0
 // Author:       Kyle Tucker
 // Author URI:   https://github.com/friartuck6000
 
